aaabc01f0077f5792fdc344dc61205d93f364ed9
Vedant Gosai <vtnt213@gmail.com>
2022-09-25 00:53:47 +0530

    ds

---
+++ Practice/main.py
@@ -0,0 +1,3 @@
+wickets = {"Tests":{"Ishant":[3,5,2,3],"Shami":[4,4,1,0],"Bumrah":[2,1,7,4]},"ODI":{"Ishant":[2,0],"Shami":[1,2]}} 
+
+wickets["ODI"]["Bumrah"] = [4,4]
---
+++ Practice/main1.py
@@ -0,0 +1,8 @@
+l = [1, 2, 3, 4, 5]
+
+def mystery(l):
+  l = l[2:]
+  return(l)
+
+mylist = [7,11,13,17,19,21]
+mystery(mylist)
